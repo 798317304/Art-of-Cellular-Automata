73d81a448bf33b132d64f3d5c8935e71377d9320
Jason Hu <jasonhu0406@yahoo.com>
2018-09-02 00:31:38 +0800

    Normal_Generator & ANN_Data 2 Completed
    I finished the adaptation of Normal_Generator on Windows and use it to generate a ANN data set of 50000 training and 5000 testing data sets.

--- Normal_Generator.py
+++ Normal_Generator.py
@@ -1,10 +1,12 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # This file serves to provide another way to generate CA map data
 # for training ANN_Judge.
 # It seems that CNN_Generator is not an efficient nor a currently
 # viable way. So I would just use normal methods.
 
 import json
+import platform
+import os
 
 from os import path
 proj_path = path.abspath('.')
@@ -15,22 +17,23 @@ from Atm import Atm
 
 
 # Set the rule to determine whether a CA map satisfies requirement
-# this rule is to make sure the the graph is a appriximately a large black triangle
+# this rule is to make sure the the graph is a approximately a large black triangle
 # in the middle.
 def determine(m):
 	for i in range(25):
 		for j in range(100):
-			if not (j > 49-4*i and j < 51+4*i):
+			if not (49-4*i < j < 51+4*i):
 				if m[i][j] == '1':
 					return False
-	if sum([ int(m[i][j]) for i in range(20, 30) for j in range(40, 60) ]) == 0:
+	if sum([int(m[k][l]) for k in range(20, 30) for l in range(40, 60) ]) == 0:
 		return False
 	return True
 
 seeds, judgements = [], []
 
 count = 0
-for i in range(55000):
+NumTrial = 50000  # The number of trial you want to be incorporated
+for i in range(int(1.01*NumTrial)):
 	a = Atm()
 	a.run()
 	
@@ -40,19 +43,28 @@ for i in range(55000):
 	judgements.append(int(determine(a.c)))
 
 	# if i//55 == 0:
-	print(i/55000)
+	print(i/(int(1.01*NumTrial)))
 
 
-with open(proj_path + '/CNN_Data/Data_2/Training_Seeds.json', 'w') as file:
-	json.dump(seeds[:50000], file)
-with open(proj_path + '/CNN_Data/Data_2/Training_Judgements.json', 'w') as file:
-	json.dump(judgements[:50000], file)
-with open(proj_path + '/CNN_Data/Data_2/Testing_Seeds.json', 'w') as file:
-	json.dump(seeds[50000:], file)
-with open(proj_path + '/CNN_Data/Data_2/Testing_Judgements.json', 'w') as file:
-	json.dump(judgements[50000:], file)
-
-
-
-
-
+if platform.platform()[0] == 'W':
+	# In adapting the program to Windows I went through a lot of detours and the
+	# line below is another way I found to get the path
+	# proj_path = os.path.split(os.path.realpath(__file__))[0]
+	print(proj_path + '\ANN_Data\Data_2\Training_Seeds.json')
+	with open( proj_path + '\ANN_Data\Data_2\Training_Seeds.json' , 'w') as file:
+		json.dump(seeds[:NumTrial], file)
+	with open(proj_path + '\ANN_Data\Data_2\Training_Judgements.json', 'w') as file:
+		json.dump(judgements[:NumTrial], file)
+	with open(proj_path + '\ANN_Data\Data_2\Testing_Seeds.json', 'w') as file:
+		json.dump(seeds[NumTrial:], file)
+	with open(proj_path + '\ANN_Data\Data_2\Testing_Judgements.json', 'w') as file:
+		json.dump(judgements[NumTrial:], file)
+else:
+	with open(proj_path + '/ANN_Data/Data_2/Training_Seeds.json', 'w') as file:
+		json.dump(seeds[:NumTrial], file)
+	with open(proj_path + '/ANN_Data/Data_2/Training_Judgements.json', 'w') as file:
+		json.dump(judgements[:NumTrial], file)
+	with open(proj_path + '/ANN_Data/Data_2/Testing_Seeds.json', 'w') as file:
+		json.dump(seeds[NumTrial:], file)
+	with open(proj_path + '/ANN_Data/Data_2/Testing_Judgements.json', 'w') as file:
+		json.dump(judgements[NumTrial:], file)
